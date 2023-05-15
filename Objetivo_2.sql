7c5cd3e6f4479101411743a94f03390a3ec437e6
William Vilchis Chiu <125768097+william100690@users.noreply.github.com>
2023-05-15 15:53:46 -0600

    Update_file

--- Objetivo_2.sql
+++ Objetivo_2.sql
@@ -2,13 +2,13 @@ use Insumos_Tecnologicos_SA;
 
 -- 1) ¿Qué tan actualizada está la información? 
 -- Seleccionamos las tablas que muestran fechas
-SELECT * FROM clientes; -- para esta tabla observamos que se repite la fehca 2015-01-01
-SELECT * from compra;-- para esta tabla observmaos que se repite la fecha 2015-01-30 
-SELECT * from gasto;-- para esta tabla observmaos que se repite la fecha 2015-01-01 
-SELECT * from venta; -- para esta tabla observamos que la fecha mas reciente es del 2019
+SELECT * FROM Clientes; -- para esta tabla observamos que se repite la fehca 2015-01-01
+SELECT * from Compras;-- para esta tabla observmaos que se repite la fecha 2015-01-30 
+SELECT * from Gastos;-- para esta tabla observmaos que se repite la fecha 2015-01-01 
+SELECT * from Ventas; -- para esta tabla observamos que la fecha mas reciente es del 2019
 
 -- 2) ¿Los datos están completos en todas las tablas?
-SELECT * from clientes;
+SELECT * from Clientes;
 SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
        SUM(CASE WHEN id="" or id = 0 or id IS NULL THEN 1 ELSE 0 END) as Nulos_id,
        sum(CASE WHEN provincia="" or id = 0 or provincia IS NULL THEN 1 ELSE 0 END) as Nulos_provincia,
@@ -24,10 +24,10 @@ SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
        sum(CASE WHEN trim(fecha_ultima_modificacion)="" or fecha_ultima_modificacion IS NULL THEN 1 ELSE 0 END) as Nulos_fecha_ultima_modificacion,
        sum(CASE WHEN usuario_ultima_modificacion="" or usuario_ultima_modificacion IS NULL THEN 1 ELSE 0 END) as Nulos_usuario_ultima_modificacion,
        sum(CASE WHEN marca_baja="" or marca_baja IS NULL THEN 1 ELSE 0 END) as Nulos_marca_baja
-FROM clientes;
+FROM Clientes;
 
 
-SELECT * from compra;
+SELECT * from Compras;
 SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
     sum(CASE WHEN id="" or id = 0 or id IS NULL THEN 1 ELSE 0 END) as Nulos_id,
     sum(CASE WHEN trim(Fecha)="" or Fecha IS NULL THEN 1 ELSE 0 END) as Nulos_Fecha,
@@ -35,10 +35,10 @@ SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
     sum(CASE WHEN Cantidad="" or Cantidad = 0 or Cantidad IS NULL THEN 1 ELSE 0 END) as Nulos_Cantidad,
     sum(CASE WHEN Precio="" or Precio = 0 or Precio IS NULL THEN 1 ELSE 0 END) as Nulos_Precio,
     sum(CASE WHEN IdProveedor="" or IdProveedor = 0 or IdProveedor IS NULL THEN 1 ELSE 0 END) as Nulos_IdProveedor
-FROM compra;
+FROM Compras;
 
 
-SELECT * from empleado;
+SELECT * from Empleados;
 SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
     sum(CASE WHEN id="" or id = 0 or id IS NULL THEN 1 ELSE 0 END) as Nulos_id,
     sum(CASE WHEN apellido="" or id = 0 or apellido IS NULL THEN 1 ELSE 0 END) as Nulos_apellido,
@@ -47,29 +47,28 @@ SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
     sum(CASE WHEN sector="" or sector IS NULL THEN 1 ELSE 0 END) as Nulos_sector,
     sum(CASE WHEN cargo="" or cargo IS NULL THEN 1 ELSE 0 END) as Nulos_cargo,
     sum(CASE WHEN salario="" or salario IS NULL THEN 1 ELSE 0 END) as Nulos_salario
-FROM empleado;
+FROM Empleados;
 
 
-SELECT * from gasto;
+SELECT * from Gastos;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
         sum(CASE WHEN IdGasto="" or IdGasto = 0 or IdGasto IS NULL THEN 1 ELSE 0 END) as Nulos_IdGasto,
         sum(CASE WHEN IdSucursal="" or IdSucursal = 0 or IdSucursal IS NULL THEN 1 ELSE 0 END) as Nulos_IdSucursal,
         sum(CASE WHEN IdTipoGasto="" or IdTipoGasto = 0 or IdTipoGasto IS NULL THEN 1 ELSE 0 END) as Nulos_IdTipoGasto,
         sum(CASE WHEN trim(Fecha)="" or Fecha IS NULL THEN 1 ELSE 0 END) as Nulos_Fecha,
         sum(CASE WHEN Monto="" or Monto = 0 or Monto IS NULL THEN 1 ELSE 0 END) as Nulos_Monto
-FROM gasto;
+FROM Gastos;
 
-
-SELECT * FROM productos;
+SELECT * FROM Productos;
 SELECT COUNT(*) as Total_datos,
        SUM(CASE WHEN id="" or id = 0 or id is null THEN 1 ELSE 0 END) as Nulos_id,
        SUM(CASE WHEN concepto="" or concepto is null THEN 1 ELSE 0 END) as Nulos_Conceto,
        SUM(CASE WHEN tipo="" or tipo is null THEN 1 ELSE 0 END) as Nulos_tipo,
        SUM(CASE WHEN precio="" or precio = 0 or precio is null THEN 1 ELSE 0 END) as Nulos_precio
-FROM productos;
+FROM Productos;
 
 
-SELECT * from proveedores;
+SELECT * from Proveedores;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id=0 or id is null THEN 1 ELSE 0 END) as Nulos_idproveedor,
     SUM(CASE WHEN nombre="" or nombre is null THEN 1 ELSE 0 END) as Nulos_nombre,
@@ -78,10 +77,10 @@ SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN provincia="" or provincia is null THEN 1 ELSE 0 END) as Nulos_provincia,
     SUM(CASE WHEN pais="" or pais is null THEN 1 ELSE 0 END) as Nulos_pais,
     SUM(CASE WHEN departamento="" or departamento is null THEN 1 ELSE 0 END) as Nulos_departamento
-FROM proveedores;
+FROM Proveedores;
 
 
-SELECT * from sucursal;
+SELECT * from Sucursales;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id = 0 or id is null THEN 1 ELSE 0 END) as Nulos_id,
     SUM(CASE WHEN sucursal="" or sucursal is null THEN 1 ELSE 0 END) as Nulos_sucursal,
@@ -90,18 +89,18 @@ SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN provincia="" or provincia is null THEN 1 ELSE 0 END) as Nulos_provincia,
     SUM(CASE WHEN latitud="" or latitud = 0 or latitud is null THEN 1 ELSE 0 END) as Nulos_latitud,
     SUM(CASE WHEN longitud="" or longitud = 0 or longitud is null THEN 1 ELSE 0 END) as Nulos_longitud
-FROM sucursal;
+FROM Sucursales;
 
 
-SELECT * from tipo_gasto;
+SELECT * from TiposGastos;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id is null THEN 1 ELSE 0 END) as Nulos_id,
     SUM(CASE WHEN descripcion="" or descripcion is null THEN 1 ELSE 0 END) as Nulos_descripcion,
     SUM(CASE WHEN monto_aproximado="" or monto_aproximado is null THEN 1 ELSE 0 END) as Nulos_monto_aproximado
-FROM tipo_gasto;
+FROM TiposGastos;
 
 
-SELECT * from venta;
+SELECT * from Ventas;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id=0 or id is null THEN 1 ELSE 0 END) as Nulos_id,
     SUM(CASE WHEN trim(Fecha)="" or Fecha is null THEN 1 ELSE 0 END) as Nulos_Fecha,
@@ -113,7 +112,7 @@ SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN IdProducto="" or IdProducto=0 or IdProducto is null THEN 1 ELSE 0 END) as Nulos_IdProducto,
     SUM(CASE WHEN Precio="" or Precio=0 or Precio=0 or Precio is null THEN 1 ELSE 0 END) as Nulos_Precio,
     SUM(CASE WHEN Cantidad="" or Cantidad=0 or Cantidad is null THEN 1 ELSE 0 END) as Nulos_Cantidad
-FROM venta;
+FROM Ventas;
 
 --3) ¿Se conocen las fuentes de los datos?
 -- Las fuentes son las tablas de excel que nos facilitó el departamento de Marketing
