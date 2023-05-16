ac0813077d47a6c49c79525908ac01c2ef244fef
William Vilchis Chiu <125768097+william100690@users.noreply.github.com>
2023-05-16 14:04:55 -0600

    Update_file

--- Objetivos/Objetivo_2.sql
+++ Objetivos/Objetivo_2.sql
@@ -13,19 +13,19 @@ SELECT * from Ventas; -- para esta tabla observamos que la fecha mas reciente es
 SELECT * from Clientes;
 SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
        SUM(CASE WHEN id="" or id = 0 or id IS NULL THEN 1 ELSE 0 END) as Nulos_id,
-       sum(CASE WHEN provincia="" or id = 0 or provincia IS NULL THEN 1 ELSE 0 END) as Nulos_provincia,
-       sum(CASE WHEN nombre_apellido="" or nombre_apellido IS NULL THEN 1 ELSE 0 END) as Nulos_nombre_apellido,
-       sum(CASE WHEN domicilio="" or domicilio IS NULL THEN 1 ELSE 0 END) as Nulos_domicilio,
-       sum(CASE WHEN telefono="" or telefono = 0 or telefono IS NULL THEN 1 ELSE 0 END) as Nulos_telefono,
-       sum(CASE WHEN edad="" or edad = 0 or edad IS NULL THEN 1 ELSE 0 END) as Nulos_edad,
-       sum(CASE WHEN localidad = "" or localidad IS NULL THEN 1 ELSE 0 END) as Nulos_localidad,
+       sum(CASE WHEN Provincia="" or id = 0 or Provincia IS NULL THEN 1 ELSE 0 END) as Nulos_provincia,
+       sum(CASE WHEN NombreApellido="" or NombreApellido IS NULL THEN 1 ELSE 0 END) as Nulos_nombre_apellido,
+       sum(CASE WHEN Domicilio="" or Domicilio IS NULL THEN 1 ELSE 0 END) as Nulos_domicilio,
+       sum(CASE WHEN Telefono="" or Telefono = 0 or Telefono IS NULL THEN 1 ELSE 0 END) as Nulos_telefono,
+       sum(CASE WHEN Edad="" or Edad = 0 or Edad IS NULL THEN 1 ELSE 0 END) as Nulos_edad,
+       sum(CASE WHEN Localidad = "" or Localidad IS NULL THEN 1 ELSE 0 END) as Nulos_localidad,
        sum(CASE WHEN X="" or X = 0 or X IS NULL THEN 1 ELSE 0 END) as Nulos_X,
        sum(CASE WHEN Y="" or Y = 0 or Y IS NULL THEN 1 ELSE 0 END) as Nulos_Y,
-       sum(CASE WHEN trim(fecha_alta)="" or fecha_alta IS NULL THEN 1 ELSE 0 END) as Nulos_fecha_alta,
-       sum(CASE WHEN usuario_alta="" or usuario_alta IS NULL THEN 1 ELSE 0 END) as Nulos_usuario_alta,
-       sum(CASE WHEN trim(fecha_ultima_modificacion)="" or fecha_ultima_modificacion IS NULL THEN 1 ELSE 0 END) as Nulos_fecha_ultima_modificacion,
-       sum(CASE WHEN usuario_ultima_modificacion="" or usuario_ultima_modificacion IS NULL THEN 1 ELSE 0 END) as Nulos_usuario_ultima_modificacion,
-       sum(CASE WHEN marca_baja="" or marca_baja IS NULL THEN 1 ELSE 0 END) as Nulos_marca_baja
+       sum(CASE WHEN trim(FechaAlta)="" or FechaAlta IS NULL THEN 1 ELSE 0 END) as Nulos_fecha_alta,
+       sum(CASE WHEN UsuarioAlta="" or UsuarioAlta IS NULL THEN 1 ELSE 0 END) as Nulos_usuario_alta,
+       sum(CASE WHEN trim(FechaUltimaModificacion)="" or FechaUltimaModificacion IS NULL THEN 1 ELSE 0 END) as Nulos_fecha_ultima_modificacion,
+       sum(CASE WHEN UsuarioUltimaModificacion="" or UsuarioUltimaModificacion IS NULL THEN 1 ELSE 0 END) as Nulos_usuario_ultima_modificacion,
+       sum(CASE WHEN MarcaBaja="" or MarcaBaja IS NULL THEN 1 ELSE 0 END) as Nulos_marca_baja
 FROM Clientes;
 
 
@@ -43,12 +43,12 @@ FROM Compras;
 SELECT * from Empleados;
 SELECT COUNT(*) as Total_datos,  -- observamos los datos faltantes
     sum(CASE WHEN id="" or id = 0 or id IS NULL THEN 1 ELSE 0 END) as Nulos_id,
-    sum(CASE WHEN apellido="" or id = 0 or apellido IS NULL THEN 1 ELSE 0 END) as Nulos_apellido,
-    sum(CASE WHEN nombre="" or nombre IS NULL THEN 1 ELSE 0 END) as Nulos_nombre,
-    sum(CASE WHEN sucursal="" or sucursal IS NULL THEN 1 ELSE 0 END) as Nulos_sucursal,
-    sum(CASE WHEN sector="" or sector IS NULL THEN 1 ELSE 0 END) as Nulos_sector,
-    sum(CASE WHEN cargo="" or cargo IS NULL THEN 1 ELSE 0 END) as Nulos_cargo,
-    sum(CASE WHEN salario="" or salario IS NULL THEN 1 ELSE 0 END) as Nulos_salario
+    sum(CASE WHEN Apellido="" or id = 0 or Apellido IS NULL THEN 1 ELSE 0 END) as Nulos_apellido,
+    sum(CASE WHEN Nombre="" or Nombre IS NULL THEN 1 ELSE 0 END) as Nulos_nombre,
+    sum(CASE WHEN Sucursal="" or Sucursal IS NULL THEN 1 ELSE 0 END) as Nulos_sucursal,
+    sum(CASE WHEN Sector="" or Sector IS NULL THEN 1 ELSE 0 END) as Nulos_sector,
+    sum(CASE WHEN Cargo="" or Cargo IS NULL THEN 1 ELSE 0 END) as Nulos_cargo,
+    sum(CASE WHEN Salario="" or Salario IS NULL THEN 1 ELSE 0 END) as Nulos_salario
 FROM Empleados;
 
 
@@ -64,41 +64,41 @@ FROM Gastos;
 SELECT * FROM Productos;
 SELECT COUNT(*) as Total_datos,
        SUM(CASE WHEN id="" or id = 0 or id is null THEN 1 ELSE 0 END) as Nulos_id,
-       SUM(CASE WHEN concepto="" or concepto is null THEN 1 ELSE 0 END) as Nulos_Conceto,
-       SUM(CASE WHEN tipo="" or tipo is null THEN 1 ELSE 0 END) as Nulos_tipo,
-       SUM(CASE WHEN precio="" or precio = 0 or precio is null THEN 1 ELSE 0 END) as Nulos_precio
+       SUM(CASE WHEN Concepto="" or Concepto is null THEN 1 ELSE 0 END) as Nulos_Conceto,
+       SUM(CASE WHEN Tipo="" or Tipo is null THEN 1 ELSE 0 END) as Nulos_tipo,
+       SUM(CASE WHEN Precio="" or Precio = 0 or Precio is null THEN 1 ELSE 0 END) as Nulos_precio
 FROM Productos;
 
 
 SELECT * from Proveedores;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id=0 or id is null THEN 1 ELSE 0 END) as Nulos_idproveedor,
-    SUM(CASE WHEN nombre="" or nombre is null THEN 1 ELSE 0 END) as Nulos_nombre,
-    SUM(CASE WHEN domicilio="" or domicilio is null THEN 1 ELSE 0 END) as Nulos_domicilio,
-    SUM(CASE WHEN ciudad="" or ciudad is null THEN 1 ELSE 0 END) as Nulos_ciudad,
-    SUM(CASE WHEN provincia="" or provincia is null THEN 1 ELSE 0 END) as Nulos_provincia,
-    SUM(CASE WHEN pais="" or pais is null THEN 1 ELSE 0 END) as Nulos_pais,
-    SUM(CASE WHEN departamento="" or departamento is null THEN 1 ELSE 0 END) as Nulos_departamento
+    SUM(CASE WHEN Nombre="" or Nombre is null THEN 1 ELSE 0 END) as Nulos_nombre,
+    SUM(CASE WHEN Domicilio="" or Domicilio is null THEN 1 ELSE 0 END) as Nulos_domicilio,
+    SUM(CASE WHEN Ciudad="" or Ciudad is null THEN 1 ELSE 0 END) as Nulos_ciudad,
+    SUM(CASE WHEN Provincia="" or Provincia is null THEN 1 ELSE 0 END) as Nulos_provincia,
+    SUM(CASE WHEN Pais="" or Pais is null THEN 1 ELSE 0 END) as Nulos_pais,
+    SUM(CASE WHEN Departamento="" or Departamento is null THEN 1 ELSE 0 END) as Nulos_departamento
 FROM Proveedores;
 
 
 SELECT * from Sucursales;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id = 0 or id is null THEN 1 ELSE 0 END) as Nulos_id,
-    SUM(CASE WHEN sucursal="" or sucursal is null THEN 1 ELSE 0 END) as Nulos_sucursal,
-    SUM(CASE WHEN direccion="" or direccion is null THEN 1 ELSE 0 END) as Nulos_direccion,
-    SUM(CASE WHEN localidad="" or localidad is null THEN 1 ELSE 0 END) as Nulos_localidad,
-    SUM(CASE WHEN provincia="" or provincia is null THEN 1 ELSE 0 END) as Nulos_provincia,
-    SUM(CASE WHEN latitud="" or latitud = 0 or latitud is null THEN 1 ELSE 0 END) as Nulos_latitud,
-    SUM(CASE WHEN longitud="" or longitud = 0 or longitud is null THEN 1 ELSE 0 END) as Nulos_longitud
+    SUM(CASE WHEN Sucursal="" or Sucursal is null THEN 1 ELSE 0 END) as Nulos_sucursal,
+    SUM(CASE WHEN Direccion="" or Direccion is null THEN 1 ELSE 0 END) as Nulos_direccion,
+    SUM(CASE WHEN Localidad="" or Localidad is null THEN 1 ELSE 0 END) as Nulos_localidad,
+    SUM(CASE WHEN Provincia="" or Provincia is null THEN 1 ELSE 0 END) as Nulos_provincia,
+    SUM(CASE WHEN Latitud="" or Latitud = 0 or Latitud is null THEN 1 ELSE 0 END) as Nulos_latitud,
+    SUM(CASE WHEN Longitud="" or Longitud = 0 or Longitud is null THEN 1 ELSE 0 END) as Nulos_longitud
 FROM Sucursales;
 
 
 SELECT * from TiposGastos;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id is null THEN 1 ELSE 0 END) as Nulos_id,
-    SUM(CASE WHEN descripcion="" or descripcion is null THEN 1 ELSE 0 END) as Nulos_descripcion,
-    SUM(CASE WHEN monto_aproximado="" or monto_aproximado is null THEN 1 ELSE 0 END) as Nulos_monto_aproximado
+    SUM(CASE WHEN Descripcion="" or Descripcion is null THEN 1 ELSE 0 END) as Nulos_descripcion,
+    SUM(CASE WHEN MontoAproximado="" or MontoAproximado is null THEN 1 ELSE 0 END) as Nulos_monto_aproximado
 FROM TiposGastos;
 
 
@@ -106,7 +106,7 @@ SELECT * from Ventas;
 SELECT COUNT(*) as total_datos,  -- observamos los datos faltantes
     SUM(CASE WHEN id="" or id=0 or id is null THEN 1 ELSE 0 END) as Nulos_id,
     SUM(CASE WHEN trim(Fecha)="" or Fecha is null THEN 1 ELSE 0 END) as Nulos_Fecha,
-    SUM(CASE WHEN trim(Fecha_Entrega)="" or Fecha_Entrega is null THEN 1 ELSE 0 END) as Nulos_Fecha_Entrega,
+    SUM(CASE WHEN trim(FechaEntrega)="" or FechaEntrega is null THEN 1 ELSE 0 END) as Nulos_Fecha_Entrega,
     SUM(CASE WHEN IdCanal="" or IdCanal=0 or IdCanal is null THEN 1 ELSE 0 END) as Nulos_IdCanal,
     SUM(CASE WHEN IdCliente="" or IdCliente=0 or IdCliente is null THEN 1 ELSE 0 END) as Nulos_IdCliente,
     SUM(CASE WHEN IdSucursal="" or IdSucursal=0 or IdSucursal is null THEN 1 ELSE 0 END) as Nulos_IdSucursal,
@@ -119,7 +119,7 @@ FROM Ventas;
 
 
 --3) ¿Se conocen las fuentes de los datos?
--- Las fuentes son las tablas de excel que nos facilitó el departamento de Marketing
+-- Las fuentes son las tablas de excel que nos facilitó el Departamento de Marketing
 
 
 
@@ -144,12 +144,12 @@ SELECT * from Empleados; --observmaos que el campo se llama id, hay que cambiarl
 
 
 
---5) Es importante revisar la consistencia y tipo de los datos
+--5) Es importante revisar la consistencia y Tipo de los datos
 
-SELECT * from Clientes; --las columnas X y Y estan en tipo varchar hay que cambiarlos a tipo decimal
-SELECT * from Empleados; --la columna salario esta en varchar hay que cambiarla a tipo decimal
-SELECT * from Productos; --la columna precio esta en varchar hay que cambiarla a tipo decimal
-SELECT * from Sucursales; --las columnas latitud y longitud estan en varchar cambiarla a tipo decimal
+SELECT * from Clientes; --las columnas X y Y estan en Tipo varchar hay que cambiarlos a Tipo decimal
+SELECT * from Empleados; --la columna Salario esta en varchar hay que cambiarla a Tipo decimal
+SELECT * from Productos; --la columna Precio esta en varchar hay que cambiarla a Tipo decimal
+SELECT * from Sucursales; --las columnas Latitud y Longitud estan en varchar cambiarla a Tipo decimal
 SELECT * from Ventas; -- las columnas de Precio y Cantidad estan en varchar, hay que cambiarlas a decimal y entera
 
 
