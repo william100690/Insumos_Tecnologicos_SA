73e91be2a13fc47dc3369822f6f7608d226e7bcd
William Vilchis Chiu <125768097+william100690@users.noreply.github.com>
2023-05-16 11:42:29 -0600

    Creación_procedimineto

---
+++ Herramientas/Procedimiento_poblar_calendario.sql
@@ -0,0 +1,32 @@
+use Insumos_Tecnologicos_SA;
+DROP PROCEDURE IF EXISTS `Llenar_dimension_calendario`;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Llenar_dimension_calendario`(IN `startdate` DATE, IN `stopdate` DATE)
+BEGIN
+    DECLARE currentdate DATE;
+    SET currentdate = startdate;
+    WHILE currentdate < stopdate DO
+        INSERT INTO calendario VALUES (
+                        YEAR(currentdate)*10000+MONTH(currentdate)*100 + DAY(currentdate),
+                        currentdate,
+                        YEAR(currentdate),
+                        MONTH(currentdate),
+                        DAY(currentdate),
+                        QUARTER(currentdate),
+                        WEEKOFYEAR(currentdate),
+                        DATE_FORMAT(currentdate,'%W'),
+                        DATE_FORMAT(currentdate,'%M'));
+        SET currentdate = ADDDATE(currentdate,INTERVAL 1 DAY);
+    END WHILE;
+END$$
+DELIMITER ;
+
+
+
+/*LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Calendario.csv' 
+INTO TABLE calendario
+FIELDS TERMINATED BY ',' ENCLOSED BY '' ESCAPED BY '' 
+LINES TERMINATED BY '\n' IGNORE 1 LINES;*/
+
+CALL Llenar_dimension_calendario('2015-01-01','2021-01-01');
+SELECT * FROM calendario;
