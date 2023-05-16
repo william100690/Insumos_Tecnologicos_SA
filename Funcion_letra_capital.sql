8cd253bea1b0c12f59caaa01231579106ad47996
William Vilchis Chiu <125768097+william100690@users.noreply.github.com>
2023-05-16 09:34:41 -0600

    Create_function

---
+++ Funcion_letra_capital.sql
@@ -0,0 +1,39 @@
+-- Homework.
+use Insumos_Tecnologicos_SA;
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+
+DROP FUNCTION IF EXISTS `UC_Words`;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `UC_Words`( str VARCHAR(255) ) RETURNS varchar(255) CHARSET utf8
+BEGIN  
+  DECLARE c CHAR(1);  
+  DECLARE s VARCHAR(255);  
+  DECLARE i INT DEFAULT 1;  
+  DECLARE bool INT DEFAULT 1;  
+  DECLARE punct CHAR(17) DEFAULT ' ()[]{},.-_!@;:?/';  
+  SET s = LCASE( str );  
+  WHILE i < LENGTH( str ) DO  
+     BEGIN  
+       SET c = SUBSTRING( s, i, 1 );  
+       IF LOCATE( c, punct ) > 0 THEN  
+        SET bool = 1;  
+      ELSEIF bool=1 THEN  
+        BEGIN  
+          IF c >= 'a' AND c <= 'z' THEN  
+             BEGIN  
+               SET s = CONCAT(LEFT(s,i-1),UCASE(c),SUBSTRING(s,i+1));  
+               SET bool = 0;  
+             END;  
+           ELSEIF c >= '0' AND c <= '9' THEN  
+            SET bool = 0;  
+          END IF;  
+        END;  
+      END IF;  
+      SET i = i+1;  
+    END;  
+  END WHILE;  
+  RETURN s;  
+END$$
+DELIMITER ;
